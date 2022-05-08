2a51018fcd8c6b3710001c0a9205df7473c6fe94
amanrajput2000 <101562671+amanrajput2000@users.noreply.github.com>
2022-05-08 17:36:16 +0530

    Update Skipendpauseresume.py

--- EvaMaria/Skipendpauseresume.py
+++ EvaMaria/Skipendpauseresume.py
@@ -14,12 +14,12 @@ async def skip(client, m: Message):
     if len(m.command) < 2:
         op = await skip_current_song(chat_id)
         if op == 0:
-            await m.reply("**🙄There's nothing in the queue to skip!**")
+            await m.reply("**𝗞𝘂𝗰𝗵 𝗰𝗵𝗮𝗹 𝗵𝗶 𝗻𝗶 𝗿𝗵𝗮 𝘁𝗼 𝗸𝘆𝗮 𝘀𝗸𝗶𝗽 𝗸𝗿𝘂 😒!**")
         elif op == 1:
-            await m.reply("**😩Empty Queue, Leaving Voice Chat**")
+            await m.reply("**😩𝗤𝘂𝗲𝘂𝗲 𝗶𝘀 𝗲𝗺𝗽𝘁𝘆, 𝗹𝗲𝗮𝘃𝗶𝗻𝗴 𝗩𝗼𝗶𝗰𝗲 𝗰𝗵𝗮𝘁**")
         else:
             await m.reply(
-                f"**⏭ Skipped** \n**🎧 Now playing** - [{op[0]}]({op[1]}) | `{op[2]}`",
+                f"**⏭ Skipped** \n**🎧 𝗡𝗼𝘄 𝗽𝗹𝗮𝘆𝗶𝗻𝗴** - [{op[0]}]({op[1]}) | `{op[2]}`",
                 disable_web_page_preview=True,
             )
     else:
@@ -48,11 +48,11 @@ async def stop(client, m: Message):
         try:
             await call_py.leave_group_call(chat_id)
             clear_queue(chat_id)
-            await m.reply("**😐End**")
+            await m.reply("**😐𝗘𝗻𝗱𝗶𝗻𝗴 𝗣𝗹𝗮𝘆𝗯𝗮𝗰𝗸𝘀**")
         except Exception as e:
-            await m.reply(f"**ERROR** \n`{e}`")
+            await m.reply(f"**𝗘𝗥𝗥𝗢𝗥** \n`{e}`")
     else:
-        await m.reply("**🤨Nothing is playing !**")
+        await m.reply("**🤨𝗞𝘂𝗰𝗵 𝗻𝗮𝗵𝗶 𝗰𝗵𝗮𝗹 𝗿𝗵𝗮!**")
 
 
 @Client.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
@@ -63,12 +63,12 @@ async def pause(client, m: Message):
         try:
             await call_py.pause_stream(chat_id)
             await m.reply(
-                f"**⏸ Paused.**\n\n• To resume playback, use the command » {HNDLR}resume"
+                f"**⏸ 𝘀𝗼𝗻𝗴 𝗶𝘀 𝗽𝗮𝘂𝘀𝗲𝗱 𝗺𝗶𝘁 𝗴𝘆𝗶 𝗸𝗵𝘂𝗷𝗹𝗶😆.**\n\n• 𝗿𝗲𝘀𝘂𝗺𝗲 𝗸𝗮𝗿𝗻𝗲 𝗸𝗲 𝗹𝗶𝘆𝗲 𝘆𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱 𝘂𝘀𝗲 𝗸𝗿𝗼😄 » {HNDLR}resume"
             )
         except Exception as e:
-            await m.reply(f"**ERROR** \n`{e}`")
+            await m.reply(f"**𝗘𝗥𝗥𝗢𝗥** \n`{e}`")
     else:
-        await m.reply("**🤨Nothing is playing!**")
+        await m.reply("**🤨𝗞𝘂𝗰𝗵 𝗻𝗵𝗶 𝗰𝗵𝗮𝗹 𝗿𝗵𝗮!**")
 
 
 @Client.on_message(filters.command(["resume"], prefixes=f"{HNDLR}"))
@@ -79,9 +79,9 @@ async def resume(client, m: Message):
         try:
             await call_py.resume_stream(chat_id)
             await m.reply(
-                f"**▶ Resumed**\n\n• To pause playback, use the command » {HNDLR}pause**"
+                f"**▶ 𝘀𝗼𝗻𝗴 𝗶𝘀 𝗿𝗲𝘀𝘂𝗺𝗲𝗱 **\n\n• 𝗳𝗶𝗿𝘀𝗲 𝗸𝗵𝘂𝗷𝗹𝗶 𝗵𝗼 𝗿𝗵𝗶 𝘁𝗼 𝗽𝗮𝘂𝘀𝗲 𝗸𝗿𝗻𝗲 𝗸𝗲 𝗹𝗶𝘆𝗲 𝘆𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱 𝘂𝘀𝗲 𝗸𝗿😄 » {HNDLR}pause**"
             )
         except Exception as e:
-            await m.reply(f"**ERROR** \n`{e}`")
+            await m.reply(f"**𝗘𝗥𝗥𝗢𝗥** \n`{e}`")
     else:
-        await m.reply("**🙄 Nothing is currently paused!**")
+        await m.reply("**🙄 𝗞𝘂𝗰𝗵 𝗻𝗵𝗶 𝗰𝗵𝗮𝗹 𝗿𝗵𝗮 𝘁𝗼 𝗸𝘆 𝗽𝗮𝘂𝘀𝗲 𝗸𝗿𝘂!**")
